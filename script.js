4c094dc06a674f5debac503f691c3c96929c81a4
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2024-09-30 22:40:40 +0600

    add javascript to files

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 console.log('Hello World!');
 
-const myName = prompt('What\'s your name?');
+// const myName = prompt('What\'s your name?');
 
-const header = document.querySelector('h1');
+// const header = document.querySelector('h1');
 
-header.innerText = `${header.innerText} of ${myName}`;
+// header.innerText = `${header.innerText} of ${myName}`;
 
+const form = document.getElementById('task-form-one');
+const taskList = [];
+const firstList = document.getElementById('task-list-one');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log('Prevented the form GET request');
+    console.log(event);
+    taskList.push(event.target[0].value);
+    const newListElements = [];
+    // debugger
+    for (const task of taskList) {
+        const listElem = document.createElement('li');
+        listElem.innerText = task;
+        newListElements.push(listElem);
+    }
+    firstList.replaceChildren(...newListElements);
+});
